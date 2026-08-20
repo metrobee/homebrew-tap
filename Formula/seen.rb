7f623df97a5dafdc54b79bf610dab20bdbf49167
metrobee <metrobee@gmail.com>
2026-08-20 23:04:41 +0300

    feat(seen): bump version to v1.3.6 with hiirkäbik and käbik taxonomy

--- Formula/seen.rb
+++ Formula/seen.rb
@@ -1,8 +1,8 @@
 class Seen < Formula
   desc "PlutoFF: Professionaalne CLI tööriist seenevaatluste edastamiseks PlutoF / eElurikkuse andmebaasi"
   homepage "https://github.com/metrobee/plutoff"
-  url "https://github.com/metrobee/plutoff/archive/refs/tags/v1.3.5.tar.gz"
-  sha256 "0953a17f7cf87296674e15116ffb193dadd9d692479c90ec7b8d7f1ce17769af"
+  url "https://github.com/metrobee/plutoff/archive/refs/tags/v1.3.6.tar.gz"
+  sha256 "2aa15b9e28aa12aefb4af0589e170d2ad8f03a393eb04660aef71d72b8922b71"
   license "MIT"
 
   def install
