7d3411b6912f8bc6a48257e03db4103158058033
metrobee <metrobee@gmail.com>
2026-08-20 10:46:41 +0300

    feat: add completion installation support to brew formulas

--- Formula/seen.rb
+++ Formula/seen.rb
@@ -1,42 +1,47 @@
 class Seen < Formula
- desc "PlutoFF: Professionaalne CLI tööriist seenevaatluste edastamiseks PlutoF / eElurikkuse andmebaasi"
- homepage "https://github.com/metrobee/plutoff"
- url "https://github.com/metrobee/plutoff/archive/refs/tags/v1.0.0.tar.gz"
- sha256 "b6947521632801bf398f5741b7829b78354666365efedacfca6495cd32a3e3d5"
- license "MIT"
+  desc "PlutoFF: Professionaalne CLI tööriist seenevaatluste edastamiseks PlutoF / eElurikkuse andmebaasi"
+  homepage "https://github.com/metrobee/plutoff"
+  url "https://github.com/metrobee/plutoff/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "b6947521632801bf398f5741b7829b78354666365efedacfca6495cd32a3e3d5"
+  license "MIT"
 
- def install
- libexec.install "seen.py", "google_photos_sync.py", "requirements.txt", ".env.example"
+  def install
+    libexec.install "seen.py", "google_photos_sync.py", "requirements.txt", ".env.example"
 
- (bin/"seen").write <<~EOS
- #!/usr/bin/env bash
- exec python3 "#{libexec}/seen.py" "$@"
- EOS
- chmod 0755, bin/"seen"
+    (bin/"seen").write <<~EOS
+      #!/usr/bin/env bash
+      exec python3 "#{libexec}/seen.py" "$@"
+    EOS
+    chmod 0755, bin/"seen"
 
- (bin/"plutoff").write <<~EOS
- #!/usr/bin/env bash
- exec python3 "#{libexec}/seen.py" "$@"
- EOS
- chmod 0755, bin/"plutoff"
- end
+    (bin/"plutoff").write <<~EOS
+      #!/usr/bin/env bash
+      exec python3 "#{libexec}/seen.py" "$@"
+    EOS
+    chmod 0755, bin/"plutoff"
 
- def caveats
- <<~EOS
- PlutoFF ('seen') on edukalt paigaldatud!
- 
- Esmakordsel seadistamisel loo fail ~/.plutof_env oma PlutoF volitustega:
- PLUTOF_CLIENT_ID=sinu_id
- PLUTOF_CLIENT_SECRET=sinu_secret
- PLUTOF_USERNAME=sinu_kasutajanimi
- PLUTOF_PASSWORD=sinu_parool
+    if File.directory?("completions")
+      zsh_completion.install "completions/seen.zsh" => "_seen" if File.exist?("completions/seen.zsh")
+      bash_completion.install "completions/seen.bash" => "seen" if File.exist?("completions/seen.bash")
+    end
+  end
 
- Käsu testimiseks käivita:
- seen --help
- EOS
- end
+  def caveats
+    <<~EOS
+      PlutoFF ('seen') on edukalt paigaldatud!
+      
+      Esmakordsel seadistamisel loo fail ~/.plutof_env oma PlutoF volitustega:
+        PLUTOF_CLIENT_ID=sinu_id
+        PLUTOF_CLIENT_SECRET=sinu_secret
+        PLUTOF_USERNAME=sinu_kasutajanimi
+        PLUTOF_PASSWORD=sinu_parool
 
- test do
- system "#{bin}/seen", "--help"
- end
+      Käsu testimiseks käivita:
+        seen --help
+    EOS
+  end
+
+  test do
+    system "#{bin}/seen", "--help"
+  end
 end
