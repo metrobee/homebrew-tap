3710e8db051fdeee2f704e6a3e2bd6cf5ed6d4b9
metrobee <metrobee@gmail.com>
2026-08-20 17:49:06 +0300

    feat(seen): bump version to v1.2.7 with genus-level (sp.) observation support

--- Formula/seen.rb
+++ Formula/seen.rb
@@ -1,8 +1,8 @@
 class Seen < Formula
   desc "PlutoFF: Professionaalne CLI tööriist seenevaatluste edastamiseks PlutoF / eElurikkuse andmebaasi"
   homepage "https://github.com/metrobee/plutoff"
-  url "https://github.com/metrobee/plutoff/archive/refs/tags/v1.2.6.tar.gz"
-  sha256 "64889a0c9d437a17895e590f8e232c3dd5726ff3bb8e425aaa324144e52943d5"
+  url "https://github.com/metrobee/plutoff/archive/refs/tags/v1.2.7.tar.gz"
+  sha256 "54053556ae5487621241a3483187ee057101cfe3c48e56ffd39dd9d3c9b9adce"
   license "MIT"
 
   def install
