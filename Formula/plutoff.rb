33c07b464c31c064f6494bb73ce7ec227026dff4
metrobee <metrobee@gmail.com>
2026-08-20 14:57:56 +0300

    feat(seen): bump version to v1.3.0 with taim botanical CLI tool

--- Formula/plutoff.rb
+++ Formula/plutoff.rb
@@ -1,12 +1,13 @@
 class Seen < Formula
-  desc "PlutoFF: Professionaalne CLI tööriist seenevaatluste edastamiseks PlutoF / eElurikkuse andmebaasi"
+  desc "PlutoFF: CLI tööriist seene- ja taimevaatluste edastamiseks PlutoF / eElurikkuse andmebaasi"
   homepage "https://github.com/metrobee/plutoff"
-  url "https://github.com/metrobee/plutoff/archive/refs/tags/v1.2.1.tar.gz"
-  sha256 "df4bce0b61e14b8285633728aad9ccc7c50eef32a47b3e0e4ac4b83b89561d3c"
+  url "https://github.com/metrobee/plutoff/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "db2a9fd9234897831f2798ccd85df1a42e9ee9d8c21b5b03d68b1700b125ec3c"
   license "MIT"
 
   def install
     libexec.install "seen.py", "requirements.txt", ".env.example" if File.exist?(".env.example")
+    libexec.install "taim.py" if File.exist?("taim.py")
     libexec.install "google_photos_sync.py" if File.exist?("google_photos_sync.py")
 
     (bin/"seen").write <<~EOS
@@ -14,6 +15,12 @@ class Seen < Formula
       exec python3 "#{libexec}/seen.py" "$@"
     EOS
     chmod 0755, bin/"seen"
+
+    (bin/"taim").write <<~EOS
+      #!/usr/bin/env bash
+      exec python3 "#{libexec}/taim.py" "$@"
+    EOS
+    chmod 0755, bin/"taim"
 
     (bin/"plutoff").write <<~EOS
       #!/usr/bin/env bash
@@ -29,7 +36,7 @@ class Seen < Formula
 
   def caveats
     <<~EOS
-      PlutoFF ('seen') on edukalt paigaldatud!
+      PlutoFF ('seen' ja 'taim') on edukalt paigaldatud!
       
       Esmakordsel seadistamisel loo fail ~/.plutof_env oma PlutoF volitustega:
         PLUTOF_CLIENT_ID=sinu_id
@@ -37,12 +44,14 @@ class Seen < Formula
         PLUTOF_USERNAME=sinu_kasutajanimi
         PLUTOF_PASSWORD=sinu_parool
 
-      Käsu testimiseks käivita:
+      Käskude testimiseks:
         seen --help
+        taim --help
     EOS
   end
 
   test do
     system "#{bin}/seen", "--help"
+    system "#{bin}/taim", "--help"
   end
 end
