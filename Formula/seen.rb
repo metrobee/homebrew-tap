10d6b81bbe48fc2668ab84eb6eaa809d1e3969e0
metrobee <metrobee@gmail.com>
2026-08-25 14:18:52 +0300

    chore(formula): bump seen formula to v1.6.1

--- Formula/seen.rb
+++ Formula/seen.rb
@@ -1,49 +1,18 @@
 class Seen < Formula
-  desc "PlutoFF: Professionaalne CLI tööriist seenevaatluste edastamiseks PlutoF / eElurikkuse andmebaasi"
+  desc "Tormiliselt kiire PlutoF seenevaatluste sisestamise CLI tööriist"
   homepage "https://github.com/metrobee/plutoff"
-  url "https://github.com/metrobee/plutoff/archive/refs/tags/v1.6.0.tar.gz"
-  sha256 "7225493325b6b126a63c27c42834ef7296e6c5e663013263c1406cd6a0c3090f"
+  url "https://github.com/metrobee/plutoff/archive/refs/tags/v1.6.1.tar.gz"
+  sha256 "d872a2105b61ebdbe0ec51153cb9bdf6a3629a827a743a6a89aaf7e1d50205ae"
   license "MIT"
 
+  depends_on "python@3.11"
+  depends_on "exiftool"
+
   def install
-    libexec.install "seen.py", "requirements.txt", ".env.example" if File.exist?(".env.example")
-    libexec.install "google_photos_sync.py" if File.exist?("google_photos_sync.py")
-
-    (bin/"seen").write <<~EOS
-      #!/usr/bin/env bash
-      exec python3 "#{libexec}/seen.py" "$@"
-    EOS
-    chmod 0755, bin/"seen"
-
-    (bin/"plutoff").write <<~EOS
-      #!/usr/bin/env bash
-      exec python3 "#{libexec}/seen.py" "$@"
-    EOS
-    chmod 0755, bin/"plutoff"
-
-    if File.directory?("completions")
-      zsh_completion.install "completions/_seen" => "_seen" if File.exist?("completions/_seen")
-      zsh_completion.install "completions/seen.zsh" => "_seen" if File.exist?("completions/seen.zsh")
-      bash_completion.install "completions/seen.bash" => "seen" if File.exist?("completions/seen.bash")
-    end
-  end
-
-  def caveats
-    <<~EOS
-      PlutoFF ('seen') on edukalt paigaldatud!
-      
-      Esmakordsel seadistamisel loo fail ~/.plutof_env oma PlutoF volitustega:
-        PLUTOF_CLIENT_ID=sinu_id
-        PLUTOF_CLIENT_SECRET=sinu_secret
-        PLUTOF_USERNAME=sinu_kasutajanimi
-        PLUTOF_PASSWORD=sinu_parool
-
-      Käsu testimiseks käivita:
-        seen --help
-    EOS
+    bin.install "seen.py" => "seen"
   end
 
   test do
-    system "#{bin}/seen", "--help"
+    system "#{bin}/seen", "--version"
   end
 end
