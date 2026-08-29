c31f70e8aecf9e395ed02bcf8345dd70ac52d50a
metrobee <metrobee@gmail.com>
2026-08-29 23:15:40 +0300

    feat(seen): bump formula to v1.6.5

--- Formula/seen.rb
+++ Formula/seen.rb
@@ -1,8 +1,8 @@
 class Seen < Formula
   desc "Tormiliselt kiire PlutoF seenevaatluste sisestamise CLI tööriist"
   homepage "https://github.com/metrobee/plutoff"
-  url "https://github.com/metrobee/plutoff/archive/refs/tags/v1.6.4.tar.gz"
-  sha256 "372045838799dcee51e9e054d69dbe2f5444b6249da10f9d02dad541fcd89913"
+  url "https://github.com/metrobee/plutoff/archive/refs/tags/v1.6.5.tar.gz"
+  sha256 "ce9523e59415f23f1ca4e3743c01c469b6a652f9202c72faa4160db25a004ca8"
   license "MIT"
 
   depends_on "python@3.11"
