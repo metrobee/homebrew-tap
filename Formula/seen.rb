7ec91730e31da05f2dfdbef536a8aa14675e6fac
metrobee <metrobee@gmail.com>
2026-09-06 21:23:28 +0300

    perf(seen): bump formula to v1.6.8 with instant async deploy & token caching

--- Formula/seen.rb
+++ Formula/seen.rb
@@ -1,8 +1,8 @@
 class Seen < Formula
   desc "Tormiliselt kiire PlutoF seenevaatluste sisestamise CLI tööriist"
   homepage "https://github.com/metrobee/plutoff"
-  url "https://github.com/metrobee/plutoff/archive/refs/tags/v1.6.7.tar.gz"
-  sha256 "66636e29a6c975b3d1d91556272817cdd746924152b454934387586eb692e04c"
+  url "https://github.com/metrobee/plutoff/archive/refs/tags/v1.6.8.tar.gz"
+  sha256 "3ff278f153a39bd186766c211c994e82b45aa96b6895dc33fedda4f98ea99c28"
   license "MIT"
 
   depends_on "python@3.11"
